668de36e0a0f68733900b04dacc612791517ff8f
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2020-05-29 16:31:31 +0200

    Adding project skeleton

---
+++ WordTarget/WordTargetCoreTests/WordTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordTargetCore
+{
+    [TestClass]
+    public class WordTest
+    {
+        [TestMethod]
+        public void WordCreation()
+        {
+            Word w = new Word();
+        }
+    }
+}
