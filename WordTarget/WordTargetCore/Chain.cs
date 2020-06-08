edb9a818d1b09306eb6cb806526bac2e2162928c
mcarcassi <matteo.c.carcassi@gmail.com>
2020-06-08 16:59:24 +0200

    Started implementing chain class

--- WordTarget/WordTargetCore/Chain.cs
+++ WordTarget/WordTargetCore/Chain.cs
@@ -6,8 +6,21 @@ namespace WordTargetCore
 {
     public class Chain
     {
-        public void add(Link catBat)
+        private List<Link> links = new List<Link>();
+        public int Count { get { return links.Count; } }
+        public Link this[int i]
         {
+            get { return links[i]; }
         }
+
+        public void add(Link newLink)
+        {   
+            if (links.Count != 0 && links[links.Count-1].WordB != newLink.WordA)
+            {
+                throw new Exception(links[links.Count - 1] + " is not a valid link with " + newLink);
+            }
+            links.Add(newLink);
+        }
+        
     }
 }
--- WordTarget/WordTargetCoreTests/ChainTest.cs
+++ WordTarget/WordTargetCoreTests/ChainTest.cs
@@ -16,6 +16,22 @@ namespace WordTargetCore
             Chain chain = new Chain();
             chain.add(catBat);
             chain.add(batBaseball);
+            Assert.AreEqual(2, chain.Count);
+            Assert.AreEqual(catBat, chain[0]);
+            Assert.AreEqual(batBaseball, chain[1]);
+        }
+        
+        [TestMethod]
+        public void InvalidChainCreation()
+        {
+            Word cat = new Word("Cat");
+            Word bat = new Word("Bat");
+            Word baseball = new Word("Baseball");
+            Link catBat = new Link(cat, bat, LinkType.OneLetterChange);
+            Link batBaseball = new Link(bat, baseball, LinkType.WordAssociation);
+            Chain chain = new Chain();
+            chain.add(batBaseball);
+            Assert.ThrowsException<System.Exception>(() => chain.add(catBat));
         }
     }
 }
