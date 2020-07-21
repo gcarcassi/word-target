74c41e3c00837e3d603fd6b10488b1aad6d85267
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2020-07-21 15:14:20 -0400

    Adding TextMesh Pro and a simple test

--- UnityUI/Assets/ButtonBehaviour.cs
+++ UnityUI/Assets/ButtonBehaviour.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using WordTargetCore;
+using TMPro;
 
 public class ButtonBehaviour : MonoBehaviour
 {
+    public TextMeshProUGUI text;
+
     int n;
     public void OnButtonPress()
     {
         n++;
         Debug.Log("New link: " + new Word("cat"));
+        Debug.Log("New element: " + text);
+        text.SetText("Changing " + n);
     }
 }
