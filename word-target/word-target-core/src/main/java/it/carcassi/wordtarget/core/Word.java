5a63d76c9a714883db2cd43968b7a84da35c0e02
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2020-08-17 17:17:25 -0400

    Adding javadoc example

--- word-target/word-target-core/src/main/java/it/carcassi/wordtarget/core/Word.java
+++ word-target/word-target-core/src/main/java/it/carcassi/wordtarget/core/Word.java
@@ -6,16 +6,30 @@
 package it.carcassi.wordtarget.core;
 
 /**
+ * Represents a word.
  *
  * @author carcassi
  */
 public class Word {
     private final String text;
 
+    /**
+     * Creates a new word with the given text, which is converted to uppercase.
+     * 
+     * @param text the text of the new word; can't be null
+     */
     public Word(String text) {
+        if (text == null)
+            throw new NullPointerException("The text of the word cannot be null");
+        
         this.text = text;
     }
 
+    /**
+     * Returns the text representation of the word, which is always uppercase.
+     * 
+     * @return the text representation; never null
+     */
     public String getText() {
         return text;
     }
